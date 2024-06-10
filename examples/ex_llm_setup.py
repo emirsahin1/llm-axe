d6e3f2d7c80fb51128049d8323eae8faa6c0474b
Emir Sahin <emirsah1@live.com>
2024-06-09 22:05:03 -0400

    Updated custom llm example

--- examples/ex_llm_setup.py
+++ examples/ex_llm_setup.py
@@ -8,10 +8,19 @@ class MyCustomLLM():
     # The prompts are in open ai prompt format
     #  example: {"role": "system", "content": "You are a helpful assistant."}
     # If your model supports json format, use the format parameter to specify that to your model.
-    def ask(self, prompts:list, format:str=""):
+    def ask(self, prompts:list, format:str="", temperature:float=0.8):
+        """
+        Args:
+            prompts (list): A list of prompts to ask.
+            format (str, optional): The format of the response. Use "json" for json. Defaults to "".
+            temperature (float, optional): The temperature of the LLM. Defaults to 0.8.
+        """
         return "Your llms response to the prompts goes here!"        
 
+
+
+# ------------------------------------------------------------------------------
 # example usage:
 llm = MyCustomLLM()
-fc = Agent(llm, agent_type=AgentType.GENERIC_RESPONDER)
-fc.ask("Hi how are you today?")
+agent = Agent(llm, agent_type=AgentType.GENERIC_RESPONDER)
+print(agent.ask("Hi how are you today?"))
