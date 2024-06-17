6e316f699917f359fe9c0ac81c3e5e9416a9ce46
yoni13 <76271912+yoni13@users.noreply.github.com>
2024-06-17 14:54:30 +0800

    Update core.py

--- core.py
+++ core.py
@@ -199,7 +199,7 @@ def fetch_url_info(url):
     Returns:
         dict: A dictionary containing the url, title and description of the website.
     """
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
     try:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
